4d18a3c2167ddbdccb85e05ac8080e23bdf9c25a
Bridget Akellian <akellianbv@gmail.com>
2022-07-06 11:21:25 +0200

    completed

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) do |dwarves, position|
+    puts "#{position}. #{dwarves}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|call| call.capitalize + "!"}
+end
+# accept an array of calls. The method should return true if any of the calls are longer than four characters. For example:
+def long_planeteer_calls(calls_long)
+  calls_long.any? {|calls| calls.length > 4}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(potentially_cheesy_items)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    maybe_cheese.include?(maybe_cheese)
+  end
 end
